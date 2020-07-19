b03e12c1cb5855e8fcb609b8f20f18cbf4eab12f
Erin Fox <erinfox@Erins-MacBook-Pro-CK.local>
2020-07-19 15:34:23 -0700

    another try at fixing the build...

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,23 @@
-module.exports = () => ({
-  plugins: [require("tailwindcss")],
-})
+// module.exports = () => ({
+//   plugins: [require("tailwindcss")],
+// })
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            // added sections folder and changed extension to jsx
+            content: [
+              "./components/**/*.jsx",
+              "./pages/**/*.js",
+              "./sections/**/**/*.jsx",
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
+  },
+}
