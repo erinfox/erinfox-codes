ddff431e412c129b9717628373e825134797d3ff
Erin Fox <erinfox@Erins-MacBook-Pro-CK.local>
2020-07-20 08:12:54 -0700

    another try

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  theme: {
+    extends: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
-  theme: {},
   variants: {},
   plugins: [],
 }
